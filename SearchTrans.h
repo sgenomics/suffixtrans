ff53e2c3cc6025e52e3beedf52d7bd90ea895447
Nava Whiteford <new@sgenomics.co.uk>
2012-05-30 14:23:46 +0100

    bug fixes to make build_disk_idx and web_disk work correctly.

--- SearchTrans.h
+++ SearchTrans.h
@@ -1,5 +1,6 @@
 #include "SuffixTree.h"
 #include <limits>
+#include "stringify.h"
 
 using namespace std;
 
@@ -7,10 +8,11 @@ class SearchTrans {
 
 public:
 
-  SearchTrans() : aggregation(1),
-                  symbol_count(0),
-                  symbol_cache(0),
-                  transcoded_bits(8) {
+  SearchTrans(bool uninit=false) : aggregation(1),
+                                   symbol_count(0),
+                                   symbol_cache(0),
+                                   transcoded_bits(8),
+                                   st(uninit) {
   }
 
   // this is a placeholder, I may use it to reduce the bit count later.
@@ -55,23 +57,20 @@ public:
 
     size_t original_position = hit*(aggregation+1);
 
-    //cout << "offset: " << offset << endl;
-    //cout << "Original Text: ";
-    //for(int n=0;n<20;n++) {
-    //  cout << original_text[original_position-offset+n];
-    //}
-    //cout << endl;
+    cout << "Original Text: ";
+    for(int n=0;n<20;n++) {
+      cout << original_text[original_position-offset+n];
+    }
+    cout << endl;
 
-    //cout << "Search string: ";
-    //for(size_t n=0;n<s.size();n++) {
-    //  cout << s[n]; 
-    //}
-    //cout << endl;
+    cout << "Search string: ";
+    for(size_t n=0;n<s.size();n++) {
+      cout << s[n]; 
+    }
+    cout << endl;
 
     //validate missing start positions
     for(int n=0;n<offset;n++) {
-      //cout << "Scomparing: " << n << " " << s[n] << endl;
-      //cout << "Ocomparing: " << original_text[original_position-offset+n] << endl;
       if(s[n] != original_text[original_position-offset+n]) return false;
     }
 
@@ -88,7 +87,7 @@ public:
     if((offset == 0) && ((s.size() % (aggregation+1)) == 0)) return;
 
     for(size_t n=0;n<hits.size();n++) {
-      bool valid = validate_hit(hits[n],s,offset);
+      bool valid = validate_hit(hits[n],s,offset); 
       if(!valid) hits[n]=numeric_limits<size_t>::max();
     }
   }
@@ -110,7 +109,6 @@ public:
       //}
 
       vector<size_t> hits = st.all_occurs(transcode(ss,offset),max_hits);
-
       validation_filter(hits,ss,offset);
       for(size_t n=0;n<hits.size();n++) if(hits[n]!=numeric_limits<size_t>::max()) all_hits.push_back((hits[n]*(aggregation+1)-offset));
     }
@@ -139,6 +137,19 @@ public:
   }
 
   void finalise() {
+    if(symbol_count == aggregation) {
+      st.insert(join(symbol_cache,0));
+      symbol_count = 0;
+      symbol_cache = 0;
+      original_text.push_back(0);
+    }
+ 
+    uint16_t t = final_symbol;
+    uint8_t t1 = t >> transcoded_bits;
+    uint8_t t2 = 0x00FF & t;
+ 
+    original_text.push_back(t1);
+    original_text.push_back(t2);
     st.finalise();
   }
   
@@ -162,13 +173,58 @@ public:
 
   suffixnodestore_type &get_store() {
     return st.store;
-  } 
+  }
 
-  uint8_t aggregation; 
-  uint8_t symbol_count;
+  void save_members(string filename) {
+    // write my members
+    ofstream membersfile(filename.c_str(),ios_base::app); // open for append
+    membersfile << "searchtrans_aggregation=" << (int) aggregation << endl;
+    membersfile << "searchtrans_symbol_count=" << (int) symbol_count << endl;
+    membersfile << "searchtrans_symbol_cache=" << (int) symbol_cache << endl;
+    membersfile << "searchtrans_transcoded_bits=" << (int) transcoded_bits << endl;
+    membersfile.close();
+
+    st.save_members(filename);
+  }
+
+  void load_members(string filename) {
+    ifstream membersfile(filename.c_str());
+  
+    for(;!membersfile.eof();) {
+      string line;
+      getline(membersfile,line);
+
+      stringstream cline(line);
+
+      string member;
+      string value;
+      getline(cline,member,'=');
+      getline(cline,value);
+
+      if(member == "searchtrans_aggregation"    ) aggregation     = convertTo<uint16_t>(value);
+      if(member == "searchtrans_symbol_count"   ) symbol_count    = convertTo<uint16_t>(value);
+      if(member == "searchtrans_symbol_cache"   ) symbol_cache    = convertTo<uint16_t>(value);
+      if(member == "searchtrans_trnascoded_bits") transcoded_bits = convertTo<uint16_t>(value);
+    }
+    membersfile.close();
+
+    st.load_members(filename);
+  }
+
+  void save_original_text(string filename) {
+    ofstream textfile(filename.c_str());
+ 
+    for(size_t n=0;n<original_text.size();n++) {
+      textfile << original_text[n];
+    }
+    textfile.close();
+  }
+
+  uint8_t  aggregation; 
+  uint8_t  symbol_count;
   uint16_t symbol_cache;
-  uint8_t transcoded_bits;
+  uint8_t  transcoded_bits;
 
-  vector<uint8_t> original_text;
+  searchtrans_store_type original_text;
   SuffixTree st;
 };
